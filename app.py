94c4fdbac8a4349c5c90ee6bdb30368aa13dc7d9
Hunter <hunter.baisden@gmail.com>
2025-03-06 21:44:49 -0500

    changes for railway

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug = False, host = "0.0.0.0",port = 80)
+    import os
+    port = int(os.getenv("PORT", 5000))  # Use PORT from Railway or default to 5000
+    app.run(debug=False, host='0.0.0.0', port=port)
